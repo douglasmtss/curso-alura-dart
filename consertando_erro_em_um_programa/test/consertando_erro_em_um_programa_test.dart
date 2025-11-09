73d761620e21065b19f2bd034943a2f9ea0a7c84
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 16:40:37 -0300

    consertando erro em um programa

---
+++ consertando_erro_em_um_programa/test/consertando_erro_em_um_programa_test.dart
@@ -0,0 +1,8 @@
+import 'package:consertando_erro_em_um_programa/consertando_erro_em_um_programa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
