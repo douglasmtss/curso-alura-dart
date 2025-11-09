a544abd13a67a62a98be19b25f1e821db787885a
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 16:14:00 -0300

    calculadora -> funções

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -4,5 +4,21 @@ void main() {
   double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
 
-  print(numeroUm + numeroDois);
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  soma();
 }
