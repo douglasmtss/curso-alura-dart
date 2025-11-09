4fdcbc467bdd6ebde30529edee438a013111b2ae
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 17:27:29 -0300

    calculadora com operações usando if

---
+++ calculadora_com_aperacao/bin/calculadora_com_aperacao.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  String operacao = stdin.readLineSync()!;
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  if (operacao == '+') {
+    soma();
+  }
+
+  if (operacao == '-') {
+    subtracao();
+  }
+
+  if (operacao == '*') {
+    multiplicacao();
+  }
+
+  if (operacao == '/') {
+    divisao();
+  }
+}
---
+++ calculadora_com_aperacao/test/calculadora_com_aperacao_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculadora_com_aperacao/calculadora_com_aperacao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
