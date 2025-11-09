2d3818c4232b092e54e1176e6bdd05ce04ead16e
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 17:00:13 -0300

    udpate ficha de partipante

--- ficha_de_participante/bin/ficha_de_participante.dart
+++ ficha_de_participante/bin/ficha_de_participante.dart
@@ -1,4 +1,3 @@
-
 void main(List<String> arguments) {
   String nome = "Matheus Marcus";
   String cpf = "100.000.000";
@@ -6,9 +5,17 @@ void main(List<String> arguments) {
   double altura = 1.76;
   bool participaDaComunidade = true;
 
-  print('Eu sou $nome,');
-  print('meu cpf é $cpf,');
-  print('sou membro da comunidade? $participaDaComunidade.');
-  print('Eu tenho $altura metros de altura e');
-  print('$idade anos de idade,');
+  // print('Eu sou $nome,');
+  // print('meu cpf é $cpf,');
+  // print('sou membro da comunidade? $participaDaComunidade.');
+  // print('Eu tenho $altura metros de altura e');
+  // print('$idade anos de idade,');
+
+  print(
+    'Eu sou $nome, \n'
+    'meu CPF é $cpf, \n'
+    'sou membro da comunidade? $participaDaComunidade. \n'
+    'Eu tenho $altura metros de altura e \n'
+    '$idade anos de idade,\n',
+  );
 }
