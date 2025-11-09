fc7c3e7ff0c45729f0a41a5874bcb6f2fd023dcf
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 17:06:09 -0300

    update clube de beneficios

--- clube_de_beneficios/bin/clube_de_beneficios.dart
+++ clube_de_beneficios/bin/clube_de_beneficios.dart
@@ -2,20 +2,40 @@ import 'dart:io';
 
 void main() {
   double pontosIniciais = 100;
-  double pontosRetirados = 0.0;
-  double pontosRestantes = 0.0;
+  double pontosRetirados;
 
   print("Você tem $pontosIniciais pontos.");
   print("Quantos pontos você gostaria de resgatar?");
+
   pontosRetirados = double.parse(stdin.readLineSync()!);
 
-  void resgatarPontos() {
-    pontosRestantes = pontosIniciais - pontosRetirados;
-  }
-
-  resgatarPontos();
+  double pontosRestantes = resgatarPontos(pontosIniciais, pontosRetirados);
 
   print(
     "Você resgatou $pontosRetirados pontos. Pontos restantes: $pontosRestantes.",
   );
 }
+
+double resgatarPontos(double pontosIniciais, double pontosRetirados) {
+  return pontosIniciais - pontosRetirados;
+}
+
+// void main() {
+//   double pontosIniciais = 100;
+//   double pontosRetirados = 0.0;
+//   double pontosRestantes = 0.0;
+
+//   print("Você tem $pontosIniciais pontos.");
+//   print("Quantos pontos você gostaria de resgatar?");
+//   pontosRetirados = double.parse(stdin.readLineSync()!);
+
+//   void resgatarPontos() {
+//     pontosRestantes = pontosIniciais - pontosRetirados;
+//   }
+
+//   resgatarPontos();
+
+//   print(
+//     "Você resgatou $pontosRetirados pontos. Pontos restantes: $pontosRestantes.",
+//   );
+// }
