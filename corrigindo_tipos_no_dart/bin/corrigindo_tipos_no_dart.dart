5d1e5d036bd8ffb789f7053af65de5eb3982090c
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 16:44:30 -0300

    corrigindo tipos no dart

---
+++ corrigindo_tipos_no_dart/bin/corrigindo_tipos_no_dart.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  double pontosIniciais = 100;
+  double pontosRetirados;
+
+  print("Você tem $pontosIniciais pontos.");
+  print("Quantos pontos você gostaria de resgatar?");
+  pontosRetirados = double.parse(stdin.readLineSync()!);
+  double pontosRestantes = pontosIniciais - pontosRetirados;
+
+  print("Você resgatou $pontosRetirados pontos. Pontos restantes: $pontosRestantes.");
+}
---
+++ corrigindo_tipos_no_dart/test/corrigindo_tipos_no_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:corrigindo_tipos_no_dart/corrigindo_tipos_no_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
