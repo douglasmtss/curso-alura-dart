6ae60915768e560fb392d1ec7014a4690d986cc5
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 16:27:51 -0300

    ficha de participante

---
+++ ficha_de_participante/bin/ficha_de_participante.dart
@@ -0,0 +1,14 @@
+
+void main(List<String> arguments) {
+  String nome = "Matheus Marcus";
+  String cpf = "100.000.000";
+  int idade = 32;
+  double altura = 1.76;
+  bool participaDaComunidade = true;
+
+  print('Eu sou $nome,');
+  print('meu cpf é $cpf,');
+  print('sou membro da comunidade? $participaDaComunidade.');
+  print('Eu tenho $altura metros de altura e');
+  print('$idade anos de idade,');
+}
---
+++ ficha_de_participante/test/ficha_de_participante_test.dart
@@ -0,0 +1,8 @@
+import 'package:ficha_de_participante/ficha_de_participante.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
