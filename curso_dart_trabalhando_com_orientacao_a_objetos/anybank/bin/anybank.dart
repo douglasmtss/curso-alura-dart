5154de2fbb91618392c398b8554c227c8e6d7ad7
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 09:28:43 -0300

    feat: implement saldo update and tax calculation in ContaEmpresa and ContaInvestimento classes

--- curso_dart_trabalhando_com_orientacao_a_objetos/anybank/bin/anybank.dart
+++ curso_dart_trabalhando_com_orientacao_a_objetos/anybank/bin/anybank.dart
@@ -4,6 +4,9 @@ void main() {
   ContaCorrente contaChris = ContaCorrente('Chris', 4000);
   ContaPoupanca contaDenize = ContaPoupanca('Denize', 4000);
 
+  ContaEmpresa contaMatheus = ContaEmpresa('Matheus', 2000);
+  ContaInvestimento contaRoberta = ContaInvestimento('Roberta', 2000);
+
   contaChris.imprimeSaldo();
   contaChris.receber(100);
 
@@ -23,4 +26,8 @@ void main() {
   contaDenize.enviar(4300);
   contaDenize.calculaRendimento();
   contaDenize.imprimeSaldo();
+
+  contaMatheus.enviar(1000);
+
+  contaRoberta.receber(1000);
 }
