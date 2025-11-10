61dbc24d23086e67096c9c46472c95b05000623f
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 09:59:34 -0300

    feat: refactor requestData to handle API response asynchronously

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -8,5 +8,10 @@ void requestData() {
   String url =
       'https://gist.githubusercontent.com/douglasmtss/11ca2d618d743b3cc6899bef6a1cfa2d/raw/5fb4258e0a73afe8ca9227dd3f879fe0c2f1fd0f/accounts.json';
 
-  print(get(Uri.parse(url)));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+    print(response);
+    print(response.body);
+  });
 }
