9cab25da070a2d45e2c8a80b831e5182272b0f87
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 09:52:51 -0300

    feat: update main function to call requestData and fetch account data from API

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -1,3 +1,12 @@
+import 'package:http/http.dart';
+
 void main() {
-  print('Olá, mundo!');
+  requestData();
+}
+
+void requestData() {
+  String url =
+      'https://gist.githubusercontent.com/douglasmtss/11ca2d618d743b3cc6899bef6a1cfa2d/raw/5fb4258e0a73afe8ca9227dd3f879fe0c2f1fd0f/accounts.json';
+
+  print(get(Uri.parse(url)));
 }
