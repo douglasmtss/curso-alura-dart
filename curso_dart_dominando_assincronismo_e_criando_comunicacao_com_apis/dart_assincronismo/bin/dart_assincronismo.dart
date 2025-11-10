865aa072f18caf4f673e379b7d74150adcd38d68
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 19:31:37 -0300

    feat: update main function to call requestDataAsync instead of requestData

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -2,7 +2,8 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() {
-  requestData();
+  // requestData();
+  requestDataAsync();
 }
 
 void requestData() {
@@ -27,4 +28,19 @@ void requestData() {
 
     print(mapCarla['balance']);
   });
+
+  print('Última coisa a acontecer na função.');
+}
+
+void requestDataAsync() async {
+  String url =
+      'https://gist.githubusercontent.com/douglasmtss/11ca2d618d743b3cc6899bef6a1cfa2d/raw/5fb4258e0a73afe8ca9227dd3f879fe0c2f1fd0f/accounts.json';
+
+  Uri uri = Uri.parse(url);
+
+  Response response = await get(uri);
+
+  print(json.decode(response.body));
+
+  print('Última coisa a acontecer na função.');
 }
