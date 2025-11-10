c8cd60e0f1e5b6d6da23afedad9ff52fff99e430
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 19:23:35 -0300

    feat: refactor requestData to improve URL parsing and enhance readability

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   requestData();
@@ -8,10 +9,22 @@ void requestData() {
   String url =
       'https://gist.githubusercontent.com/douglasmtss/11ca2d618d743b3cc6899bef6a1cfa2d/raw/5fb4258e0a73afe8ca9227dd3f879fe0c2f1fd0f/accounts.json';
 
-  Future<Response> futureResponse = get(Uri.parse(url));
+  Uri uri = Uri.parse(url);
+
+  Future<Response> futureResponse = get(uri);
+
   print(futureResponse);
+
   futureResponse.then((Response response) {
     print(response);
     print(response.body);
+
+    List<dynamic> listAccounts = json.decode(response.body);
+
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (element) => element['name'].toString().toLowerCase() == 'carla',
+    );
+
+    print(mapCarla['balance']);
   });
 }
