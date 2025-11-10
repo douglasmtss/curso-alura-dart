4a0189f9eb4530a1a1b886531578aaa832059949
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 08:52:20 -0300

    feat: initialize gatos_e_cachorros project with basic structure and functionality

---
+++ curso_dart_trabalhando_com_orientacao_a_objetos/conta_salario/bin/conta_salario.dart
@@ -0,0 +1,5 @@
+import 'package:conta_salario/conta_salario.dart' as conta_salario;
+
+void main(List<String> arguments) {
+  print('Hello world: ${conta_salario.calculate()}!');
+}
---
+++ curso_dart_trabalhando_com_orientacao_a_objetos/gatos_e_cachorros/test/gatos_e_cachorros_test.dart
@@ -0,0 +1,8 @@
+import 'package:gatos_e_cachorros/gatos_e_cachorros.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
