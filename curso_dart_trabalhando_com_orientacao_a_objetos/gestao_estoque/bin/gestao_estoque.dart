65555c66a008c40a5920f2d13f97efc72a590379
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 08:14:22 -0300

    feat: add price attribute to stock items and update item listing format

--- curso_dart_trabalhando_com_orientacao_a_objetos/gestao_estoque/bin/gestao_estoque.dart
+++ curso_dart_trabalhando_com_orientacao_a_objetos/gestao_estoque/bin/gestao_estoque.dart
@@ -1,13 +1,15 @@
 void main() {
   List<Map<String, Object>> items = [
-    {'nome': 'Smartphone', 'quantidade': 100},
-    {'nome': 'Notebook', 'quantidade': 50},
+    {'preco': 2000, 'nome': 'Smartphone', 'quantidade': 100},
+    {'preco': 7000, 'nome': 'Notebook', 'quantidade': 50},
+    {'preco': 1000, 'nome': 'Impressora', 'quantidade': 20},
   ];
 
   List<ItemEstoque> itemsEstoque = [];
 
   for (Map<String, Object> item in items) {
     ItemEstoque itemEstoque = ItemEstoque(
+      double.parse(item['preco'].toString()),
       item['nome'].toString(),
       int.parse(item['quantidade'].toString()),
     );
@@ -15,23 +17,32 @@ void main() {
     itemsEstoque.add(itemEstoque);
   }
 
+  print('\nListagem inicial');
   for (ItemEstoque item in itemsEstoque) {
-    print('Produto: ${item.nome}, Quantidade em estoque: ${item.quantidade}');
+    item.imprimir();
   }
 
   itemsEstoque[0].setQuantidade(20); // Smartphone
   itemsEstoque[1].retirada(10); // Notebook
 
+  print('\nAlterações em Smartphone e Notebook');
   for (ItemEstoque item in itemsEstoque) {
-    print('Produto: ${item.nome}, Quantidade em estoque: ${item.quantidade}');
+    item.imprimir();
+  }
+
+  itemsEstoque[2].setPreco(1250);
+  print('\nAlterações em Impressora');
+  for (ItemEstoque item in itemsEstoque) {
+    item.imprimir();
   }
 }
 
 class ItemEstoque {
+  double preco;
   String nome;
   int quantidade;
 
-  ItemEstoque(this.nome, this.quantidade);
+  ItemEstoque(this.preco, this.nome, this.quantidade);
 
   void setQuantidade(int? quantidade) {
     if (quantidade != null && quantidade > 0) {
@@ -44,5 +55,17 @@ class ItemEstoque {
       this.quantidade -= quantidade;
     }
     return this.quantidade;
+  }
+
+  void setPreco(double? preco) {
+    if (preco != null && preco > 0.0) {
+      this.preco = preco;
+    }
+  }
+
+  void imprimir() {
+    print(
+      'Produto: $nome, preço: R\$${preco.toStringAsFixed(2)}, Quantidade em estoque: $quantidade',
+    );
   }
 }
