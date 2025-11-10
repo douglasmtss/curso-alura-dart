fcf455c75948f2f68dc813c3e45ae97e47a71e6b
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 22:51:45 -0300

    refactor: remove commented-out code in validacao_tipo_de_arquivos.dart

--- validacao_tipo_de_arquivos/bin/validacao_tipo_de_arquivos.dart
+++ validacao_tipo_de_arquivos/bin/validacao_tipo_de_arquivos.dart
@@ -19,3 +19,23 @@ void main() {
 
   validateTipoArquivo();
 }
+
+// void main() {
+//   List<String> tipos = ['pdf', 'jpg', 'png', 'docx'];
+//   String? tipo;
+
+//   tipo = validaTipo(tipo, tipos);
+
+//   print('Tipo válido: $tipo');
+// }
+
+// String? validaTipo(String? tipo, List<String> tipos) {
+//   print('Digite o tipo do arquivo (pdf, jpg, png, docx):');
+//   tipo = stdin.readLineSync();
+//   if (tipos.contains(tipo)) {
+//     return tipo;
+//   } else {
+//     print('Tipo inválido. Tente novamente.');
+//     return validaTipo(tipo, tipos);
+//   }
+// }
