4e54d314505d046973d16d874e22e9a9e318bd72
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 08:41:19 -0300

    feat: add ContaCorrente and ContaPoupanca classes for account management

--- curso_dart_trabalhando_com_orientacao_a_objetos/anybank/bin/anybank.dart
+++ curso_dart_trabalhando_com_orientacao_a_objetos/anybank/bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta('Matheus', 1000);
   Conta contaMaria = Conta('Maria', 2000);
+  ContaCorrente contaChris = ContaCorrente('Chris', 4000);
+  ContaPoupanca contaDenize = ContaPoupanca('Denize', 4000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaMaria];
 
@@ -13,4 +15,10 @@ void main() {
   contaMaria.receber(3000);
   contaMatheus.receber(500);
   contaMatheus.enviar(200);
+
+  contaChris.imprimeSaldo();
+  contaChris.receber(100);
+
+  contaDenize.imprimeSaldo();
+  contaDenize.enviar(300);
 }
