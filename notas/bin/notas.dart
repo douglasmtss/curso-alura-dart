3964b76effc17c7e79b3a2fe2c89f2ed69f61179
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 23:22:29 -0300

    refactor: streamline main function to utilize menu for user interaction

--- notas/bin/notas.dart
+++ notas/bin/notas.dart
@@ -2,15 +2,12 @@ import 'dart:io';
 
 void main() {
   List<String> notas = <String>[];
-  getComando();
-  adicionNota(notas);
-  adicionNota(notas);
-  adicionNota(notas);
-  listarNotas(notas);
+
+  menu(notas);
 }
 
 String getComando() {
-  print('Digite um comando: 1 - Adicionar nota, 2 - Listar notas, e - Sair');
+  print('Digite um comando: 1-Adicionar nota, 2-Listar notas, 3-Sair');
 
   List<String> comandos = <String>['1', '2', '3'];
 
@@ -45,5 +42,20 @@ List<String> adicionNota(List<String> notas) {
 void listarNotas(List<String> notas) {
   for (var i = 0; i < notas.length; i++) {
     print(notas[i]);
+  }
+}
+
+void menu(List<String> notas) {
+  String comando = getComando();
+
+  switch (comando) {
+    case '1':
+      adicionNota(notas);
+      menu(notas);
+    case '2':
+      listarNotas(notas);
+      menu(notas);
+    case '3':
+      print('Até breve!');
   }
 }
