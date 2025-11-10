ce90cd1561d46741eea24a6d7418301f03dc40da
douglasmtss <douglasmatosdev@gmail.com>
2025-11-09 22:27:56 -0300

    feat: add initial implementation of meses_do_ano_recursao application with basic functionality and tests

---
+++ meses_do_ano_recursao/bin/meses_do_ano_recursao.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+void main() {
+  void mesesDoAno() {
+    print('Digite um número de 1 a 12 para saber o mês correspondente:');
+    String? input = stdin.readLineSync();
+
+    // Null safety e conversão de entrada
+    int numero = int.parse(input!);
+
+    // Função que retorna o mês correspondente
+    String obterMes(int numero) {
+      switch (numero) {
+        case 1:
+          return 'Janeiro';
+        case 2:
+          return 'Fevereiro';
+        case 3:
+          return 'Março';
+        case 4:
+          return 'Abril';
+        case 5:
+          return 'Maio';
+        case 6:
+          return 'Junho';
+        case 7:
+          return 'Julho';
+        case 8:
+          return 'Agosto';
+        case 9:
+          return 'Setembro';
+        case 10:
+          return 'Outubro';
+        case 11:
+          return 'Novembro';
+        case 12:
+          return 'Dezembro';
+        default:
+          print('Número inválido. Por favor, insira um número de 1 a 12.');
+          mesesDoAno();
+          return '';
+      }
+    }
+
+    String mes = obterMes(numero);
+    print(mes);
+  }
+
+  mesesDoAno();
+}
---
+++ meses_do_ano_recursao/test/meses_do_ano_recursao_test.dart
@@ -0,0 +1,8 @@
+import 'package:meses_do_ano_recursao/meses_do_ano_recursao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
