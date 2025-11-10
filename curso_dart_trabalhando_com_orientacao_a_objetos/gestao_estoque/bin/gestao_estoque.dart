53ec5156a8c3f9144482b9043f85fb25c656ff71
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 08:14:48 -0300

    refactor: comment out unused ItemEstoque class and main function for clarity

--- curso_dart_trabalhando_com_orientacao_a_objetos/gestao_estoque/bin/gestao_estoque.dart
+++ curso_dart_trabalhando_com_orientacao_a_objetos/gestao_estoque/bin/gestao_estoque.dart
@@ -69,3 +69,48 @@ class ItemEstoque {
     );
   }
 }
+
+// class ItemEstoque {
+//   String nomeProduto;
+//   int quantidade;
+//   double preco;
+
+//   ItemEstoque(this.nomeProduto, this.quantidade, this.preco);
+
+//   void registrarEntrada(int valor) {
+//     quantidade += valor;
+//   }
+
+//   void registrarSaida(int valor) {
+//     if (quantidade >= valor) {
+//       quantidade -= valor;
+//     } else {
+//       print('Quantidade insuficiente em estoque para realizar a saída.');
+//     }
+//   }
+
+//   void atualizarPreco(double novoPreco) {
+//     preco = novoPreco;
+//   }
+// }
+
+// void main() {
+//   // Instanciando os itens
+//   ItemEstoque smartphone = ItemEstoque('Smartphone', 100, 2999.99);
+//   ItemEstoque notebook = ItemEstoque('Notebook', 50, 4999.99);
+//   ItemEstoque tablet = ItemEstoque('Tablet', 75, 1999.99);
+
+//   // Atualizando o preço do Notebook
+//   notebook.atualizarPreco(4799.99);
+
+//   // Imprimindo informações atualizadas dos itens
+//   print(
+//     'Produto: ${smartphone.nomeProduto}, Quantidade em estoque: ${smartphone.quantidade}, Preço: R\$${smartphone.preco}',
+//   );
+//   print(
+//     'Produto: ${notebook.nomeProduto}, Quantidade em estoque: ${notebook.quantidade}, Preço: R\$${notebook.preco}',
+//   );
+//   print(
+//     'Produto: ${tablet.nomeProduto}, Quantidade em estoque: ${tablet.quantidade}, Preço: R\$${tablet.preco}',
+//   );
+// }
