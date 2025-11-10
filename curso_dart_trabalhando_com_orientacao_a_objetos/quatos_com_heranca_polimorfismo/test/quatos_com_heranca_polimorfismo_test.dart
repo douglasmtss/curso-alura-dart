eaa9ac54e4def95160333348473fd678f90bda27
douglasmtss <douglasmatosdev@gmail.com>
2025-11-10 08:58:28 -0300

    feat: add initial project files including README, CHANGELOG, analysis options, and main application logic

---
+++ curso_dart_trabalhando_com_orientacao_a_objetos/quatos_com_heranca_polimorfismo/test/quatos_com_heranca_polimorfismo_test.dart
@@ -0,0 +1,8 @@
+import 'package:quatos_com_heranca_polimorfismo/quatos_com_heranca_polimorfismo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
