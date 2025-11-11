bb9533b44c0095305179e81542a5cbd9b87a4846
douglasmtss <douglasmatosdev@gmail.com>
2025-11-11 16:10:37 -0300

    Agendando consultas para uma clínica veterinária

---
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/agendando_consultas_clinica_veterinaria/test/agendando_consultas_clinica_veterinaria_test.dart
@@ -0,0 +1,8 @@
+import 'package:agendando_consultas_clinica_veterinaria/agendando_consultas_clinica_veterinaria.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
