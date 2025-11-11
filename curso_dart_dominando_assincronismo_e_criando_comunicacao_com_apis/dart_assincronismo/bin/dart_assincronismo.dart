27ed9c3a178d84e46bf6de79e734186b9686316e
douglasmtss <douglasmatosdev@gmail.com>
2025-11-11 16:49:09 -0300

    Refactor API interaction: update requestDataAsync to return data and modify sendDataAsync to include authorization header

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -1,9 +1,16 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'api_key.dart';
 
 void main() {
   // requestData();
-  requestDataAsync();
+  // requestDataAsync();
+  sendDataAsync({
+    'id': 'NEW001',
+    'name': 'Flutter',
+    'lastName': 'Dart',
+    'balance': 5000,
+  });
 }
 
 void requestData() {
@@ -32,7 +39,7 @@ void requestData() {
   print('Última coisa a acontecer na função.');
 }
 
-void requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       'https://gist.githubusercontent.com/douglasmtss/11ca2d618d743b3cc6899bef6a1cfa2d/raw/5fb4258e0a73afe8ca9227dd3f879fe0c2f1fd0f/accounts.json';
 
@@ -40,7 +47,31 @@ void requestDataAsync() async {
 
   Response response = await get(uri);
 
-  print(json.decode(response.body));
+  return json.decode(response.body);
+}
 
-  print('Última coisa a acontecer na função.');
+void sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+
+  listAccounts.add(mapAccount);
+
+  String content = json.encode(listAccounts);
+
+  String url = 'https://api.github.com/gists/11ca2d618d743b3cc6899bef6a1cfa2d';
+
+  Uri uri = Uri.parse(url);
+
+  Response response = await post(
+    uri,
+    headers: {'Authorization': 'Bearer $githubApiKey'},
+    body: json.encode({
+      'description': 'accounts.json',
+      'public': true,
+      'files': {
+        'accounts.json': {'content': content},
+      },
+    }),
+  );
+
+  print(response.statusCode);
 }
