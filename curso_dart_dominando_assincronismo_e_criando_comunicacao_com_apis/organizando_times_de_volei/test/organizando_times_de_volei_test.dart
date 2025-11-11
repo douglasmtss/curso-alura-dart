ca552513d2ad5a3f0b26ca0acd332e5fc5eef66c
douglasmtss <douglasmatosdev@gmail.com>
2025-11-11 16:08:39 -0300

    Organizando times de vôlei por nível de habilidade

---
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/organizando_times_de_volei/test/organizando_times_de_volei_test.dart
@@ -0,0 +1,8 @@
+import 'package:organizando_times_de_volei/organizando_times_de_volei.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
