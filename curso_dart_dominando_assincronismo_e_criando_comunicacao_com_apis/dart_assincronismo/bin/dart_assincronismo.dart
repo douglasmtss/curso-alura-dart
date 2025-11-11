37442cb837f3180ddd50e7a65589498bf1d521f0
douglasmtss <douglasmatosdev@gmail.com>
2025-11-11 19:49:43 -0300

    Fix import statement for api_key and add Account model class with factory and toMap methods

--- curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
+++ curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/dart_assincronismo/bin/dart_assincronismo.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:http/http.dart';
 import 'dart:convert';
-import 'api_key.dart';
+import 'package:dart_assincronismo/api_key.dart';
 
 StreamController<String> streamController = StreamController<String>();
 
